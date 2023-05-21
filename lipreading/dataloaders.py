5.22 commit - mse loss - validation code - melspec-audio test codes - sound normalization - preprocessing fix (audio-visual)

lipreading/dataloaders.py
@@ -61,9 +61,13 @@ def get_preprocessing_pipelines(modality):
                                     AddNoise( noise=np.load('./data/babbleNoise_resample_16K.npy')),
                                     NormalizeUtterance()])
 
-        audio_preprocessing['val'] = NormalizeUtterance()
+        audio_preprocessing['val'] = Compose([
+                                    AddNoise( noise=np.load('./data/babbleNoise_resample_16K.npy')),
+                                    NormalizeUtterance()])
 
-        audio_preprocessing['test'] = NormalizeUtterance()
+        audio_preprocessing['test'] = Compose([
+                                    AddNoise( noise=np.load('./data/babbleNoise_resample_16K.npy')),
+                                    NormalizeUtterance()])
 
 
         preprocessing['audio'] = audio_preprocessing
@@ -120,7 +124,7 @@ def get_data_loaders(args):
 
 def unit_test_data_loader(args): ## unit test dataloader for multimodal(av) loading.
     preprocessing = get_preprocessing_pipelines(args.modality)
-    subset_indices = range(256)  # Specify the indices for the desired subset
+    subset_indices = range(48032)  # Specify the indices for the desired subset
     if args.modality == "av":
         partitions = ['test'] if args.test else ['train','val','test']
         dsets = {partition:Subset(AVDataset(

lipreading/dataset.py
@@ -7,7 +7,7 @@ import numpy as np
 import sys
 from lipreading.utils import read_txt_lines
 from lipreading.preprocess import NormalizeUtterance
-
+import torchaudio.transforms as transforms
 
 class MyDataset(object):
     def __init__(self, modality, data_partition, data_dir, label_fp, annonation_direc=None,
@@ -341,7 +341,7 @@ class AVDataset(object): # dataset for multi-modal training
             boundary = self._get_boundary(self.audio_list[idx][0], raw_data)
             return preprocess_data, label, boundary
         else:
-            return audio_preprocess_data,video_preprocess_data,audio_data # we don't need label. we need original audio data without mixup!
+            return audio_preprocess_data,video_preprocess_data,audio_raw_data # we don't need label. we need original audio data without mixup!
 
     def __len__(self):
         return len(self._audio_data_files) # video will be same
@@ -377,7 +377,7 @@ def pad_packed_collate(batch):
         return data, lengths, labels
 
 def av_pad_packed_collate(batch): ## our av collate function #TODO implement faster tensor transforms
-    if len(batch[0]) == 3:
+    if len(batch[0]) == 3: # audio_preprocessed,video_preprocessed,audio_raw
         use_boundary = False
         audio_data_tuple, audio_lengths, video_data_tuple, video_lengths,audio_raw_data = zip(*[(a, a.shape[0], b, b.shape[0],c) for (a, b,c) in sorted(batch, key=lambda x: x[0].shape[0], reverse=True)])
     
@@ -387,7 +387,7 @@ def av_pad_packed_collate(batch): ## our av collate function #TODO implement fas
     for idx in range( len(audio_raw_data)):
         audio_raw_data[idx][:audio_raw_data[idx].shape[0]] = audio_raw_data[idx]
 
-
+    #audio_raw_mel = np.array(mel_transform(audio_raw_data)) # transform padded audio_raw_data into melspectrogram
     max_len = audio_data_tuple[0].shape[0]
     audio_data_np = np.zeros((len(audio_data_tuple), max_len))
 
@@ -400,9 +400,30 @@ def av_pad_packed_collate(batch): ## our av collate function #TODO implement fas
     for idx in range( len(video_data_np)):
         video_data_np[idx][:video_data_tuple[idx].shape[0]] = video_data_tuple[idx]
 
-    
-    audio_data = torch.FloatTensor(audio_data_np) ##TODO need to transform ndarray to set off arrays for faster tensor transform
-    video_data = torch.FloatTensor(video_data_np)
-    audio_raw_data = torch.FloatTensor(audio_raw_data)
 
-    return audio_data, video_data, audio_lengths, video_lengths,audio_raw_data
+    # Create a PyTorch tensor from the list of NumPy arrays
+    # audio_data_list = [torch.from_numpy(arr,dtype=torch.float) for arr in audio_data_np]
+    # audio_data = torch.stack(audio_data_list)
+
+    # video_data_list = [torch.from_numpy(arr,dtype=torch.float) for arr in video_data_np]
+    # video_data = torch.stack(video_data_list)
+
+    # audio_raw_data_list = [torch.from_numpy(arr,dtype=torch.float) for arr in audio_raw_data]
+    # audio_raw_data = torch.stack(audio_raw_data_list)
+    audio_data = torch.FloatTensor(np.array(audio_data_np)) ##TODO need to transform ndarray to set off arrays for faster tensor transform
+    video_data = torch.FloatTensor(np.array(video_data_np))
+    audio_raw_data = torch.FloatTensor(np.array(audio_raw_data))
+    #audio_raw_mel = torch.FloatTensor(audio_raw_mel)
+    audio_raw_mel = mel_transform(audio_raw_data)
+
+
+    return audio_data, video_data, audio_lengths, video_lengths,audio_raw_mel#,audio_raw_data # temp 
+
+def mel_transform(batch_data): ## transform audio_raw_data into mel_spectrogram => something's wrong #TODO
+    mel_trans = transforms.MelSpectrogram(
+                sample_rate = 16000,
+                n_fft=1024,
+                hop_length=145,
+                n_mels=128
+            )
+    return mel_trans(batch_data)[:,:,:,128]

lipreading/model.py
@@ -367,14 +367,25 @@ class AVLipreading(nn.Module): ## new model - audio-visual cross attention
 
             # audio feature extraction
             # (B,1,18560)
-            B, C, T = audio_data.size()
+            B, C, T = audio_data.size() ## audio is not normalized (-1,1)
+            #print("audio_data - max",torch.max(audio_data))
             ## mel-spectogram generation
             # audio_data = (B,18560),normalized tensor #TODO : must isolate mel-spectogram generation from forward computation
+            #print(torch.max(audio_data))
             
             mel_spec = self.mel_transform(audio_data.squeeze()) # generated mel-spectogram
+            #print(torch.max(mel_spec))
             #print(mel_spec.shape)
             ###
             
+            # (b,1,18560) (32,1,18560)
+            audio_data = audio_data.squeeze() ## normalize
+            mean = torch.mean(audio_data,dim=1,keepdim=True)
+            std = torch.std(audio_data,dim=1,keepdim=True)
+            audio_data = (audio_data - mean)/std 
+            audio_data = audio_data.unsqueeze(1)
+            
+            #print(torch.max(audio_data))
             audio_data = self.audio_trunk(audio_data)
             audio_data = audio_data.transpose(1, 2) # (B, T, 512)
             audio_lengths = [_//640 for _ in audio_lengths] 
@@ -409,7 +420,7 @@ class AVLipreading(nn.Module): ## new model - audio-visual cross attention
             #print("raw = ",mel_spec.shape)
             mask = mask.squeeze()
             out = torch.mul(mask,mel_spec[:,:,:128]) # element-wise multiplication (mask, original) - mel-spectogram
-            #print("out = ",out.shape)
+            
         return out
 
 

lipreading/preprocess.py
@@ -145,14 +145,15 @@ class NormalizeUtterance():
     def __call__(self, signal):
         signal_std = 0. if np.std(signal)==0. else np.std(signal)
         signal_mean = np.mean(signal)
-        return (signal - signal_mean) / signal_std
+        #return (signal - signal_mean) / signal_std
+        return signal ## temporal code - not using normalization!
 
 
 class AddNoise(object):
     """Add SNR noise [-1, 1]
     """
     
-    def __init__(self, noise, snr_levels=[-5, 0, 5, 10, 15, 20, 9999]):
+    def __init__(self, noise, snr_levels=[-5, 0, 5, 10, 15, 20,9999]): 
         assert noise.dtype in [np.float32, np.float64], "noise only supports float data type"
         
         self.noise = noise

main.py
@@ -12,12 +12,17 @@ import random
 import argparse
 import numpy as np
 from tqdm import tqdm
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
 import soundfile as sf
+from torchmetrics.audio.stoi import ShortTimeObjectiveIntelligibility
+import torchaudio.transforms as transforms
+import librosa
+
 
 from lipreading.utils import get_save_folder
 from lipreading.utils import load_json, save2npz
@@ -28,7 +33,7 @@ from lipreading.model import Lipreading, AVLipreading
 from lipreading.mixup import mixup_data, mixup_criterion
 from lipreading.optim_utils import get_optimizer, CosineScheduler
 from lipreading.dataloaders import get_data_loaders, get_preprocessing_pipelines, unit_test_data_loader
-
+from lipreading.dataset import mel_transform
 
 def load_args(default_config=None):
     parser = argparse.ArgumentParser(description='Pytorch Lipreading ')
@@ -142,8 +147,79 @@ def evaluate(model, dset_loader, criterion):
     print(f"{len(dset_loader.dataset)} in total\tCR: {running_corrects/len(dset_loader.dataset)}")
     return running_corrects/len(dset_loader.dataset), running_loss/len(dset_loader.dataset)
 
+def mel_to_wav(mel): ##TODO something's wrong with this (backward propagation problem, torchaudio.transforms uses SGD or whatever)
+    invMel_trans = transforms.InverseMelScale(
+        sample_rate = 16000,
+        n_stft = 1024 // 2 + 1,
+    ).to(device)
+    waveform = invMel_trans(mel)
+    return waveform
+
+
 
 ##TODO need to implement multimodal evaluate function
+def multimodal_eval(model, dset_loader, criterion):
+    model.eval()
+    running_loss = 0.
+    running_stoi = 0.
+
+    with torch.no_grad():
+        for batch_idx, data in enumerate(tqdm(dset_loader)):
+            audio_data,video_data,audio_lengths,video_lengths,audio_raw_mel = data
+            # for multiple gpus
+            audio_lengths = [audio_lengths[0]]*(len(audio_lengths)//(torch.cuda.device_count()))
+            video_lengths = [video_lengths[0]]*(len(video_lengths)//(torch.cuda.device_count()))
+            #temp = mel_transform(audio_data.detach().numpy()) 
+            audio_data = audio_data.unsqueeze(1).to(device) 
+            video_data = video_data.unsqueeze(1).to(device)
+            audio_raw_mel = audio_raw_mel.to(device)
+            #print(audio_raw_mel.shape)
+            logits = model(audio_data,video_data, audio_lengths,video_lengths)
+            # audio_raw_mel.requires_grad = True
+            # logits.requires_grad = True
+            # label_wav = mel_to_wav(audio_raw_mel)
+            # pred_wav = mel_to_wav(logits)
+            # plt.figure(figsize=(10, 4))
+            # plt.imshow(torch.log(temp.detach().cpu()[0]), aspect='auto', origin='lower')
+            # plt.colorbar(format='%+2.0f dB')
+            # plt.title('Mel Spectrogram')
+            # plt.xlabel('Frames')
+            # plt.ylabel('Mel Filterbanks')
+            # plt.tight_layout()
+            # plt.savefig('./audio_noise_mel.png')
+            # plt.figure(figsize=(10, 4))
+            # plt.imshow(torch.log(logits.detach().cpu()[0]), aspect='auto', origin='lower')
+            # plt.colorbar(format='%+2.0f dB')
+            # plt.title('Mel Spectrogram')
+            # plt.xlabel('Frames')
+            # plt.ylabel('Mel Filterbanks')
+            # plt.tight_layout()
+            # plt.savefig('./logits.png')
+            # plt.figure(figsize=(10, 4))
+            # plt.imshow(torch.log(audio_raw_mel.detach().cpu()[0]), aspect='auto', origin='lower')
+            # plt.colorbar(format='%+2.0f dB')
+            # plt.title('Mel Spectrogram')
+            # plt.xlabel('Frames')
+            # plt.ylabel('Mel Filterbanks')
+            # plt.tight_layout()
+            # plt.savefig('./audio_raw_mel.png')
+            #waveform1 = librosa.feature.inverse.mel_to_audio(logits[0].detach().cpu().numpy(),sr=16000,n_fft=1024,hop_length =145) # i
+            #waveform2 = librosa.feature.inverse.mel_to_audio(temp[0],sr=16000,n_fft=1024,hop_length=145)
+            #sf.write('logit.wav', waveform1, 16000)
+            #sf.write('audio_noise.wav', waveform2, 16000)
+            #sf.write('test4.wav',audio_data.squeeze().detach().cpu().numpy()[0],16000)
+            
+            #exit()
+            
+            #stoi_metric = ShortTimeObjectiveIntelligibility(sr=16000,extended=False)
+            loss = criterion(logits,audio_raw_mel) # mse
+            running_loss += loss.item() * audio_data.size(0)
+            #running_stoi += stoi_metric(pred_wav,label_wav).item() ## TODO need to implement STOI calculation (mask-ground_truth)
+
+    print(f"{len(dset_loader.dataset)} in total\tCR: {running_stoi /len(dset_loader.dataset)}")
+    return running_stoi/len(dset_loader.dataset), running_loss/len(dset_loader.dataset)
+        
+
 def multimodal_train(model, dset_loader, criterion, epoch, optimizer, logger):
     data_time = AverageMeter()
     batch_time = AverageMeter()
@@ -161,7 +237,7 @@ def multimodal_train(model, dset_loader, criterion, epoch, optimizer, logger):
 
     end = time.time()
     for batch_idx, data in enumerate(dset_loader):
-        audio_data,video_data,audio_lengths,video_lengths,audio_raw_data = data
+        audio_data,video_data,audio_lengths,video_lengths,audio_raw_mel = data
         # measure data loading time
         data_time.update(time.time() - end)
 
@@ -174,28 +250,30 @@ def multimodal_train(model, dset_loader, criterion, epoch, optimizer, logger):
 
         # (32, 1, 29, 88, 88)
         # (32, 1, 18560)
-        sf.write('original_audio.wav',audio_data[0],16000) ##TODO need to erase this saving codes .
+        #sf.write('original_audio.wav',audio_data[0],16000) ##TODO need to erase this saving codes .
         audio_data = audio_data.unsqueeze(1).to(device)
         video_data = video_data.unsqueeze(1).to(device)
-        audio_raw_data = audio_raw_data.to(device)
-        
+        audio_raw_mel = audio_raw_mel.to(device)
+        #print(audio_raw_mel.shape)
         logits = model(audio_data,video_data, audio_lengths,video_lengths)
-        print(logits) #is model working properly?
-        print(logits.shape)
-        torch.save(logits[0],'./masked_audio.pt') # save logit value to play.. ## TODO make function to sample masked & original sound file for testing.
-        plt.figure(figsize=(10, 4))
-        plt.imshow(torch.log(logits.detach().cpu()[0]), aspect='auto', origin='lower')
-        plt.colorbar(format='%+2.0f dB')
-        plt.title('Mel Spectrogram')
-        plt.xlabel('Frames')
-        plt.ylabel('Mel Filterbanks')
-        plt.tight_layout()
-        plt.savefig('./masked_mel.png')
-        exit()
-        #print(logits) #is model working properly?
 
-        loss_func = mixup_criterion(labels_a, labels_b, lam)
-        loss = loss_func(criterion, logits)
+        #functionalize this part to check mel-spectrogram every 10 epoch
+        # print(logits) #is model working properly?
+        # print(logits.shape)
+        # torch.save(logits[0],'./masked_audio.pt') # save logit value to play.. ## TODO make function to sample masked & original sound file for testing.
+        # plt.figure(figsize=(10, 4))
+        # plt.imshow(torch.log(logits.detach().cpu()[0]), aspect='auto', origin='lower')
+        # plt.colorbar(format='%+2.0f dB')
+        # plt.title('Mel Spectrogram')
+        # plt.xlabel('Frames')
+        # plt.ylabel('Mel Filterbanks')
+        # plt.tight_layout()
+        # plt.savefig('./masked_mel.png')
+        # exit()
+        # #print(logits) #is model working properly?
+
+        #loss_func = mixup_criterion(labels_a, labels_b, lam)
+        loss = criterion(logits,audio_raw_mel)
 
         loss.backward()
         optimizer.step()
@@ -205,12 +283,11 @@ def multimodal_train(model, dset_loader, criterion, epoch, optimizer, logger):
         end = time.time()
         # -- compute running performance
         _, predicted = torch.max(F.softmax(logits, dim=1).data, dim=1)
-        running_loss += loss.item()*input.size(0)
-        running_corrects += lam * predicted.eq(labels_a.view_as(predicted)).sum().item() + (1 - lam) * predicted.eq(labels_b.view_as(predicted)).sum().item()
-        running_all += input.size(0)
+        running_loss += loss.item()*audio_data.size(0)
+        running_all += audio_data.size(0)
         # -- log intermediate results
         if batch_idx % args.interval == 0 or (batch_idx == len(dset_loader)-1):
-            update_logger_batch( args, logger, dset_loader, batch_idx, running_loss, running_corrects, running_all, batch_time, data_time )
+            update_logger_batch(args, logger, dset_loader, batch_idx, running_loss, 0, running_all, batch_time, data_time ) ## TODO STOI implementation?
 
     return model
 
@@ -338,7 +415,7 @@ def get_model_from_json():
 def main():
 
     # -- logging
-    save_path = get_save_folder( args)
+    save_path = get_save_folder(args)
     print(f"Model and log being saved in: {save_path}")
     logger = get_logger(args, save_path)
     ckpt_saver = CheckpointSaver(save_path)
@@ -365,12 +442,16 @@ def main():
         model = nn.DataParallel(model)
     
     model.to(device)
-    print(device)
+    ## model size check
+    # tot_param = sum(p.numel()for p in model.parameters())
+    # print("total_params = ",tot_param)
+    # exit()
+    
     # -- get dataset iterators
-    dset_loaders = get_data_loaders(args) 
-    #dset_loaders = unit_test_data_loader(args)
+    #dset_loaders = get_data_loaders(args) 
+    dset_loaders = unit_test_data_loader(args) # using subset of dataset (currently 48032)
     # -- get loss function
-    criterion = nn.CrossEntropyLoss() ## TODO : Implement STOI Loss function
+    criterion = nn.MSELoss() if args.modality =="av" else nn.CrossEntropyLoss() 
     # -- get optimizer
     optimizer = get_optimizer(args, optim_policies=model.parameters())
     # -- get learning rate scheduler
@@ -395,7 +476,10 @@ def main():
             return
         # if test-time, performance on test partition and exit. Otherwise, performance on validation and continue (sanity check for reload)
         if args.test:
-            acc_avg_test, loss_avg_test = evaluate(model, dset_loaders['test'], criterion)
+            if args.modality == "av":
+                acc_avg_test, loss_avg_test = multimodal_eval(model, dset_loaders['test'], criterion)    
+            else:
+                acc_avg_test, loss_avg_test = evaluate(model, dset_loaders['test'], criterion)
             logger.info(f"Test-time performance on partition {'test'}: Loss: {loss_avg_test:.4f}\tAcc:{acc_avg_test:.4f}")
             return
 
@@ -411,7 +495,10 @@ def main():
         else:
             model = train(model, dset_loaders['train'], criterion, epoch, optimizer, logger) # optimize?
         
-        acc_avg_val, loss_avg_val = evaluate(model, dset_loaders['val'], criterion)
+        if args.modality =="av":
+            acc_avg_val, loss_avg_val = multimodal_eval(model,dset_loaders['val'],criterion)
+        else:
+            acc_avg_val, loss_avg_val = evaluate(model, dset_loaders['val'], criterion)
         logger.info(f"{'val'} Epoch:\t{epoch:2}\tLoss val: {loss_avg_val:.4f}\tAcc val:{acc_avg_val:.4f}, LR: {showLR(optimizer)}")
         # -- save checkpoint
         save_dict = {
@@ -426,7 +513,10 @@ def main():
     # -- evaluate best-performing epoch on test partition
     best_fp = os.path.join(ckpt_saver.save_dir, ckpt_saver.best_fn)
     _ = load_model(best_fp, model)
-    acc_avg_test, loss_avg_test = evaluate(model, dset_loaders['test'], criterion)
+    if args.modality == "av":
+        acc_avg_test, loss_avg_test = multimodal_eval(model, dset_loaders['test'], criterion)
+    else:
+        acc_avg_test, loss_avg_test = evaluate(model, dset_loaders['test'], criterion)
     logger.info(f"Test time performance of best epoch: {acc_avg_test} (loss: {loss_avg_test})")
 
 if __name__ == '__main__':

melspec_test.py
@@ -4,26 +4,33 @@ import torchaudio.transforms as transforms
 import numpy as np
 import matplotlib.pyplot as plt
 import time
+from scipy.io import wavfile
+#sample_rate = 16000  
+
 audio_path = 'datasets\\audio_data\\ABOUT\\train\\ABOUT_00001.npz'
 temp = np.load(audio_path)
 temp = torch.tensor(temp['data'])
-mel_transform = transforms.MelSpectrogram(
-    sample_rate = 16000,
-    n_fft=400,
-    hop_length=640,
-    n_mels=128
-)
-s1 = time.time()
-mel_specs = mel_transform(temp)
-s2 = time.time() - s1
-print(s2)
+#temp = temp['data']
+#wavfile.write('output.wav', sample_rate,temp)
+print(torch.max(temp),torch.min(temp))
+print(temp)
+# mel_transform = transforms.MelSpectrogram(
+#     sample_rate = 16000,
+#     n_fft=400,
+#     hop_length=640,
+#     n_mels=128
+# )
+# s1 = time.time()
+# mel_specs = mel_transform(temp)
+# s2 = time.time() - s1
+# print(s2)
 
-## visulization
-plt.figure(figsize=(10, 4))
-plt.imshow(torch.log(mel_specs), aspect='auto', origin='lower')
-plt.colorbar(format='%+2.0f dB')
-plt.title('Mel Spectrogram')
-plt.xlabel('Frames')
-plt.ylabel('Mel Filterbanks')
-plt.tight_layout()
-plt.show()
+# ## visulization
+# plt.figure(figsize=(10, 4))
+# plt.imshow(torch.log(mel_specs), aspect='auto', origin='lower')
+# plt.colorbar(format='%+2.0f dB')
+# plt.title('Mel Spectrogram')
+# plt.xlabel('Frames')
+# plt.ylabel('Mel Filterbanks')
+# plt.tight_layout()
+# plt.show()

test.py
@@ -1,7 +1,8 @@
-# import os
-# import numpy as np
-# path = 'datasets\audio_data\ABOUT\train\ABOUT_00001.npz'
-# data = np.load(path)
-# print(data)
 import torch
-print(torch.cuda.device_count())
+from model import AVLipreading
+from main import get_model_from_json
+model_path = "./train_logs/tcn/2023-05-21T17;03;02/ckpt.pth"
+av_model = get_model_from_json()
+model = torch.load(model_path)
+loaded_state_dict = checkpoint['model_state_dict']
+model = torch.load_state_dict(loaded_state_dict)